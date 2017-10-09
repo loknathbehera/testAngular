41ceec1f0b36fbc27ce66e3ebda937c0766402f8
Lokanath Behera <lb.dvlpr@gmail.com>
2017-10-09 22:40:29 +0530

    Editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
+import { NgModel } from '@angular/forms/src/directives';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details</h2>
+  <div><label>Id :</label>{{hero.id}}</div>
+ <div>
+ <label>Name:</label>
+ <input [(ngModel)] = "hero.name" placeholder="name">
+ </div>
+
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Hello App';
+  hero: Hero = {
+    id: 1,
+    name: 'Lokanath'
+  };
+}
+export class Hero {
+  id: number;
+ name: String;
 }
