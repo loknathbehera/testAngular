7e55a3a0dd75d6efe1d16ac9711d687f9ce99b4b
Lokanath Behera <lb.dvlpr@gmail.com>
2017-10-20 07:17:47 +0530

    https://angular.io/tutorial/toh-pt5#add-a-dashboard yet to start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import {HeroesComponent} from './heroes.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
   bootstrap: [ AppComponent ]
 })
